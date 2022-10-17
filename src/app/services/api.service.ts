b6f7f175dc0aba0cc68148df5c645958976fe1b5
jose99segura <jose99segura@gmail.com>
2022-10-17 14:20:51 +0200

    si

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http
 import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
 import { retry, catchError  } from 'rxjs/operators';
-// import { Usuario } from '../models/usuario';
+import { Parte } from '../models/parte';
 
 @Injectable({
   providedIn: 'root'
@@ -10,71 +10,69 @@ import { retry, catchError  } from 'rxjs/operators';
 export class ApiService {
 
   //API path
-  // eslint-disable-next-line @typescript-eslint/naming-convention
   public base_path = 'http://localhost:3000/usuarios';
 
   public httpOptions = {
     headers: new HttpHeaders({
-      // eslint-disable-next-line @typescript-eslint/naming-convention
       'Content-Type': 'application/json'
     })
   };
 
   constructor( private http: HttpClient ) { }
 
-  // handleError(error: HttpErrorResponse){
-  //   if (error.error instanceof ErrorEvent) {
-  //     console.error('Ha ocurrido un error');
-  //   }else{
-  //     console.error('nose');
+  handleError(error: HttpErrorResponse){
+    if (error.error instanceof ErrorEvent) {
+      console.error('Ha ocurrido un error');
+    }else{
+      console.error('nose');
 
-  //   }
-  //   return throwError('Ha sucedido un problema');
-  // }
+    }
+    return throwError('Ha sucedido un problema');
+  }
 
-  // createItem(item): Observable<Usuario> {
-  //   return this.http
-  //     .post<Usuario>(this.base_path, JSON.stringify(item), this.httpOptions)
-  //     .pipe(
-  //       retry(2),
-  //       catchError(this.handleError)
-  //     )
-  // }
+  createItem(item): Observable<Parte> {
+    return this.http
+      .post<Parte>(this.base_path, JSON.stringify(item), this.httpOptions)
+      .pipe(
+        retry(2),
+        catchError(this.handleError)
+      )
+  }
 
-  // getItem(id): Observable<Usuario> {
-  //   return this.http
-  //     .get<Usuario>(this.base_path + '/' + id)
-  //     .pipe(
-  //       retry(2),
-  //       catchError(this.handleError)
-  //     )
-  // }
+  getItem(id): Observable<Parte> {
+    return this.http
+      .get<Parte>(this.base_path + '/' + id)
+      .pipe(
+        retry(2),
+        catchError(this.handleError)
+      )
+  }
 
-  // getList(): Observable<Usuario> {
-  //   return this.http
-  //     .get<Usuario>(this.base_path)
-  //     .pipe(
-  //       retry(2),
-  //       catchError(this.handleError)
-  //     )
-  // }
+  getList(): Observable<Parte> {
+    return this.http
+      .get<Parte>(this.base_path)
+      .pipe(
+        retry(2),
+        catchError(this.handleError)
+      )
+  }
 
-  // updateItem(id, item): Observable<Usuario> {
-  //   return this.http
-  //     .put<Usuario>(this.base_path + '/' + id, JSON.stringify(item), this.httpOptions)
-  //     .pipe(
-  //       retry(2),
-  //       catchError(this.handleError)
-  //     )
-  // }
+  updateItem(id, item): Observable<Parte> {
+    return this.http
+      .put<Parte>(this.base_path + '/' + id, JSON.stringify(item), this.httpOptions)
+      .pipe(
+        retry(2),
+        catchError(this.handleError)
+      )
+  }
 
-  // deleteItem(id): Observable<Usuario> {
-  //   return this.http
-  //     .delete<Usuario>(this.base_path + '/' + id, this.httpOptions)
-  //     .pipe(
-  //       retry(2),
-  //       catchError(this.handleError)
-  //     )
-  // }
+  deleteItem(id): Observable<Parte> {
+    return this.http
+      .delete<Parte>(this.base_path + '/' + id, this.httpOptions)
+      .pipe(
+        retry(2),
+        catchError(this.handleError)
+      )
+  }
 
 }
