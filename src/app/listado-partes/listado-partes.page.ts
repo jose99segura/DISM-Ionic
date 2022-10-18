83b4a74de2bdc24b5689199275ae68e4f38d6d1c
jose99segura <jose99segura@gmail.com>
2022-10-18 11:17:06 +0200

    Revert "si"
    This reverts commit b6f7f175dc0aba0cc68148df5c645958976fe1b5.

--- src/app/listado-partes/listado-partes.page.ts
+++ src/app/listado-partes/listado-partes.page.ts
@@ -1,42 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { NavigationExtras, Router } from '@angular/router';
-import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-listado-partes',
   templateUrl: './listado-partes.page.html',
   styleUrls: ['./listado-partes.page.scss'],
 })
-export class ListadoPartesPage {
+export class ListadoPartesPage implements OnInit {
 
-  ParteData: any;
-  constructor( public apiService: ApiService, private router: Router ) {
-    this.ParteData = [];
+  constructor() { }
+
+  ngOnInit() {
   }
-
-  ionViewWillEnter(){
-    this.getAllPartes();
-  }
-
-  getAllPartes(){
-    this.apiService.getList().subscribe(response =>{
-      this.ParteData = response;
-    });
-  }
-
-  delete(item){
-    this.apiService.deleteItem(item.id).subscribe(response => {
-      this.ParteData = response;
-    });
-  }
-
-  // editar(item){
-  //   let navigationExtras: NavigationExtras = {
-  //     state: {
-  //       usuario: item
-  //     }
-  //   };
-  //   this.router.navigate(['editar'], navigationExtras);
-  // }
 
 }
