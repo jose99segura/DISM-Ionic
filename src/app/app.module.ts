e4e6524c48fd3dad5db8ec3fff3855316a6fb901
jose99segura <jose99segura@gmail.com>
2022-11-14 11:56:16 +0100

    s

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,14 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ApiService } from './services/api.service';
 import { Geolocation } from '@awesome-cordova-plugins/geolocation/ngx';
+import { NativeGeocoder, NativeGeocoderResult, NativeGeocoderOptions } from '@awesome-cordova-plugins/native-geocoder/ngx';
+
 
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
-  providers: [Geolocation,{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, ApiService],
+  providers: [Geolocation,NativeGeocoder, { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, ApiService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/ver-partes/ver-partes.page.ts
+++ src/app/ver-partes/ver-partes.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, NavigationExtras, Router } from '@angular/router';
+import { NativeGeocoder, NativeGeocoderResult, NativeGeocoderOptions } from '@awesome-cordova-plugins/native-geocoder/ngx';
 import { Parte } from '../models/parte';
 import { ApiService } from '../services/api.service';
 
@@ -12,8 +13,15 @@ export class VerPartesPage implements OnInit {
 
   id: number;
   data: Parte;
+  georeferenciacion;
+
+  public options: NativeGeocoderOptions = {
+    useLocale: true,
+    maxResults: 5
+  };
 
   constructor(
+    private nativeGeocoder: NativeGeocoder,
     public apiService: ApiService,
     public activatedRoute: ActivatedRoute,
     public router: Router,
@@ -27,6 +35,9 @@ export class VerPartesPage implements OnInit {
         this.data = this.router.getCurrentNavigation().extras.state.usuario;
       }
     });
+    this.georef();
+    console.log("geor: ",this.georeferenciacion);
+
   }
 
   editar(item){
@@ -36,6 +47,20 @@ export class VerPartesPage implements OnInit {
       }
     };
     this.router.navigate(['editar-parte'], navigationExtras);
+  }
+
+
+  // GEOREF
+  georef(){
+  this.nativeGeocoder.reverseGeocode(this.data.latitud, this.data.latitud, this.options)
+  .then((result: NativeGeocoderResult[]) => this.georeferenciacion = (JSON.stringify(result[0])))
+  .catch((error: any) => console.log(error));
+  console.log(this.georeferenciacion);
+
+
+  this.nativeGeocoder.forwardGeocode('Berlin', this.options)
+  .then((result: NativeGeocoderResult[]) => console.log('The coordinates are latitude=' + result[0].latitude + ' and longitude=' + result[0].longitude))
+  .catch((error: any) => console.log(error));
   }
 
 }
